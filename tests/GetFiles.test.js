b0f4f350280b6831ce8d723066e792b9baa550e0
ibrahim hamshari <lomeseth1@gmail.com>
2024-07-13 13:37:55 +0300

    init x3

--- tests/GetFiles.test.js
+++ tests/GetFiles.test.js
@@ -1,7 +1,14 @@
 import { createFile, getFiles } from '../utils/Files.js'
 test('Get Files', async () => {
-    const data = await getFiles("./dataTest");
+    const data = await getFiles("../dataTest");
+    console.log(data);
     expect(data).toEqual([{name:'File.txt', type:"file"}, { name: 'testing', type:"directory" }])
+})
+
+test('Get Files x2', async () => {
+    const data = await getFiles("../dataTest/testing");
+    console.log(data);
+    expect(data).toEqual([{name:'2.txt', type:"file"}])
 })
 
 
